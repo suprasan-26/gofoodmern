c2975bb39cb3d71db55581e08bb06df6fd16f8e3
suprasan-26 <141128484+suprasan-26@users.noreply.github.com>
2024-11-21 20:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
